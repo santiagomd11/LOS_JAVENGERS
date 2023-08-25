458516d945cc483dd216b9e52a8cadd1a578b69e
LeonelStevenLondono <leonel-londono@javeriana.edu.co>
2023-08-25 14:25:14 -0500

    Adding property with inheritance to house and vehicle

---
+++ src/property/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Property(models.Model):
+    cost  = models.IntegerField()
+    description = models.CharField(max_length=255)
+    photos = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    class meta:
+        abstrasct = True
+
+class House(Property):
+    squareMetters = models.IntegerField()
+    stratus = models.IntegerField()
+    roomsNumber = models.IntegerField()
+    bathsNumber = models.IntegerField()
+    location = models.CharField(max_length=255)
+
+    
+class Vehicle(Property):
+    mileage = models.IntegerField()
+    model = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    motor = models.CharField(max_length=255)
+# Create your models here.
