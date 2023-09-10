bb894439bc07a52fa8b64c00043d5848a37fa0a4
santiagomd11 <santiagoe4333@gmail.com>
2023-09-09 20:20:53 -0500

    Adding model for user authentication

---
+++ src/userAutentication/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+# Create your models here.
+
+
+class UserAutentication(models):
+    legalDocumentNumber = models.CharField(max_length=10)
+    legalDocumentPhoto = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    userPhoto = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
